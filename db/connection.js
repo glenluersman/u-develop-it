31551870740e17d45edd504f026dcd9da9e06d71
glenluersman <glen.luersman@gmail.com>
2022-02-07 18:39:32 -0500

    dotenv

--- db/connection.js
+++ db/connection.js
@@ -1,14 +1,16 @@
 const mysql = require('mysql2');
 
+require('dotenv').config();
+
 // Connect to database
 const db = mysql.createConnection(
     {
-        host: 'localhost',
+        host: process.env.DB_HOST,
         // Your Mysql username
-        user: 'root',
+        user: process.env.DB_USER,
         // Your Mysql password
-        password: '@legendsNeverDie21',
-        database: 'election'
+        password: process.env.DB_PW,
+        database: process.env.DB_NAME
     },
     console.log('Connected to the election database')
 );
